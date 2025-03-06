e5847771ac8b14f0dabdd89dab698b310b57a0bc
Dillon Brigode <chilldill00@Dillons-MacBook-Pro.local>
2025-03-06 12:51:57 -0500

    Inital project setup with space to implement schema and scripts. -Dillon Brigode

---
+++ scripts/build_db.sh
@@ -0,0 +1,11 @@
+
+
+# Delete old DB if it exists
+[ -f 3241.db ] && rm 3241.db
+
+# Create a new DB by running our SQL scripts
+# SCRIPTS GO HERE TO BUILD DB
+sqlite3 3241.db < sql/schema.sql
+sqlite3 3241.db < 
+
+echo "Tht data is BUILT."
